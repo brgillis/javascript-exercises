ec139e9b6b95f40db8639206d00bb61e306eaaad
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-04 14:45:05 +0100

    Implement solution for exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(iYear) {
+  if (iYear % 400 === 0) return true;
+  if (iYear % 100 === 0) return false;
+  if (iYear % 4 === 0) return true;
+  return false;
 };
 
 // Do not edit below this line
