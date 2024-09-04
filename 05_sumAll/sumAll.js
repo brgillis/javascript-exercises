083686c9ea4701a8993094ef55aca0f063a8ae9d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-04 14:27:21 +0100

    Implement basic solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(iLow, iHigh) {
+  let iResult = 0;
+  for (let i = iLow; i<=iHigh; ++i) {
+    iResult += i;
+  }
+  return iResult;
 };
 
 // Do not edit below this line
