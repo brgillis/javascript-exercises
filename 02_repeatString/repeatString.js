73ecb0c1592ff8a873eff6607cc2904e3f92eb18
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-04 13:53:55 +0100

    Implement basic solution for exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(sInputString, iNumTimes) {
+  let sOutputString = "";
+  for (let i = 0; i < iNumTimes; ++i) {
+    sOutputString += sInputString;
+  }
+  return sOutputString;
 };
 
 // Do not edit below this line
