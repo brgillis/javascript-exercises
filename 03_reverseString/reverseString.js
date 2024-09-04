83d01758a5ee93fed67e2b03eec9c7fc3495f74d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-04 14:04:01 +0100

    Implement basic solution for exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(sInput) {
+  // Build the reversed string letter-by-letter
+  let sOutput = "";
+  for (i = sInput.length-1; i>=0; --i)
+  {
+    sOutput += sInput[i];
+  }
+  return sOutput;
 };
 
 // Do not edit below this line
