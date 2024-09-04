ed9210db6e49d5bbc61dc5dacf8c63d202901bff
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-04 14:10:00 +0100

    Implment basic solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(aInput, oToRemove) {
+  // Use the array's `splice` method to remove elements found to equal the element to remove
+  for(i=0; i<aInput.length; ++i) {
+    if (aInput[i]===oToRemove) {
+      aInput.splice(i, 1);
+    }
+  }
+
+  return aInput;
 };
 
 // Do not edit below this line
