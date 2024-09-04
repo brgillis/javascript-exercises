dac38a3b5e411e2b3fb095ac01fa9adf1883b8ad
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-04 14:31:58 +0100

    Fix to work with larger number first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,10 @@
 const sumAll = function(iLow, iHigh) {
+
+  // Check for valid order, and swap if not
+  if(iHigh<iLow){
+    [iLow, iHigh] = [iHigh, iLow];
+  }
+
   let iResult = 0;
   for (let i = iLow; i<=iHigh; ++i) {
     iResult += i;
