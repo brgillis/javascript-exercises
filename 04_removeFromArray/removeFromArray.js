7fa622fad9c41aa6aaf547651c48ee0045dd8d03
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-04 14:22:39 +0100

    Fix to handle multiple arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,17 @@
-const removeFromArray = function(aInput, oToRemove) {
+const removeFromArray = function(aInput) {
+  // Get an array of the objects to remove from the function's `arguments`, which starts after
+  // the first argument.
+  const aToRemove = Array.from(arguments);
+  aToRemove.shift();
+
   // Use the array's `splice` method to remove elements found to equal the element to remove
-  for(i=0; i<aInput.length; ++i) {
-    if (aInput[i]===oToRemove) {
-      aInput.splice(i, 1);
+  for(let i=0; i<aInput.length; ++i) {
+    for(let j=0; j<aToRemove.length; ++j) {
+      if (aInput[i]===aToRemove[j]) {
+        aInput.splice(i, 1);
+        --i; // Since we remove one element, we'll need to go back one index
+        break; // No need to check remaining elements
+      }
     }
   }
 
