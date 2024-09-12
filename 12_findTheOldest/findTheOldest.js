43fd0f45e6dff3949246fbde8b8e265fe1c87984
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-12 15:54:44 +0100

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+function getAgeDiff(personA, personB) {
+  let iADeathYear = personA.yearOfDeath;
+  let iBDeathYeay = personB.yearOfDeath;
 
+  // If the death year is undefined, set it to the current year to calculate age
+  if(iADeathYear==undefined)
+    iADeathYear = 2024;
+  if(iBDeathYeay==undefined)
+    iBDeathYeay = 2024;
+
+  return iADeathYear - personA.yearOfBirth - iBDeathYeay + personB.yearOfBirth;
+}
+
+const findTheOldest = function(a) {
+  return a.reduce((personA, personB) => {
+    let ageDiff = getAgeDiff(personA, personB);
+    if (ageDiff>=0)
+      return personA;
+    else
+      return personB;
+  })
 };
 
 // Do not edit below this line
