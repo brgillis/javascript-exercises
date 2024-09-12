8e538cd4faf6cfc851182d04c8d95b66c99a9ada
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-12 15:32:01 +0100

    Complete palindromes task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+  a = a.replace(/[^0-9a-z]/gi, '')
+  for(let i=0; i<a.length; ++i)
+  {
+    if (a[i].toLowerCase() != a.at(-i-1).toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
