9d17abede22b16335f6cbb05e0a511a05132e7e9
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-12 15:18:21 +0100

    Complete calculator implementation

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((cumSum, next) => cumSum + next, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+	return a.reduce((cumProd, next) => cumProd * next, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let res = 1;
+  for(let x=a; x>0; --x) {
+    res *= x;
+  }
+  return res;
 };
 
 // Do not edit below this line
