621c8eb428cd35d12668475b063fdf243ad69383
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-12 15:37:39 +0100

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+  x = +x;
+  if(x<0) return "OOPS";
 
+  // We get exponential growth in complexity if we simply do this recursively.
+  // This can be avoided by constructing an array up to the number we need
+  a = [0, 1, 1];
+  a.length = x+1;
+  for (let i=2; i<=x; ++i)
+  {
+    a[i] = a[i-1] + a[i-2];
+  }
+  return a[x];
 };
 
 // Do not edit below this line
